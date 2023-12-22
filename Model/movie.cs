4f8f2652446afbe64eaacce2ef497a861cd8f1cc
Atharv Chothave <127308873+AtharvaChothave22@users.noreply.github.com>
2023-12-22 13:47:04 +0530

    login page through .net
    22/12/23

---
+++ Model/movie.cs
@@ -0,0 +1,8 @@
+namespace HR;
+public class movie{
+    public string mname{get;set;}
+        public string hero{get;set;}
+    public string director{get;set;}
+    public string song{get;set;}
+
+    }
