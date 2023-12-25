07d1535255ea35f979c7598eb4233666f128a3d0
Atharv Chothave <127308873+AtharvaChothave22@users.noreply.github.com>
2023-12-26 01:08:27 +0530

    Adnin db

---
+++ Admin.sql
@@ -0,0 +1,18 @@
+CREATE table ADMIN(
+ int AdminiD NOT NULL,
+ username VARCHAR(50) NOT NULL,
+ PASSWORD VARCHAR(20)  NOT NUll,
+ Role VARCHAR(30) NOT NUll,
+ emailid VARCHAR(30) NOT Null,
+ status VARCHAR(40) NOT Null,
+ clientid  int NOT Null,
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
