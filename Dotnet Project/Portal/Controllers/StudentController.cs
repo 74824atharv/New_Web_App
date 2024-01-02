0b9a956dd8da7c24d9f872c12fe2787dbaa57187
unknown <atharvachothave3@gmail.com>
2024-01-02 17:41:14 +0530

    dotnet project

---
+++ Dotnet Project/Portal/Controllers/StudentController.cs
@@ -0,0 +1,71 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Portal.Models;
+using Bll;
+using BOL;
+using DAL;
+
+namespace Portal.Controllers;
+
+public class StudentController : Controller
+{
+    private readonly ILogger<StudentController> _logger;
+
+    public StudentController(ILogger<StudentController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult StudentName()
+    {  
+        List<Students> sl=StudentManger.GetallStudents();
+   
+        ViewData["Sfamily"]=sl;
+        return View();
+    }
+     public IActionResult StudentDetails()
+    {  
+        List<Students> sl=StudentManger.GetallStudDetails();
+   
+        ViewData["Mfamily"]=sl;
+        return View();
+    }
+      public IActionResult StudentID()
+    {  
+        List<Students> sl=StudentManger.GetallStudID();
+   
+        ViewData["Mfamily"]=sl;
+        return View();
+    }
+     public IActionResult Edit()
+    {  
+       return View();
+    }
+    [HttpPost]
+   public IActionResult Edit(string fname,string lname,int SId)
+    {  
+        Console.WriteLine("in edit");
+        //int tempid=int.Parse(SId);
+       DBmanager.Update(fname,lname,SId);
+       return View();
+    }
+    public IActionResult Delete()
+    {  
+       return View();
+    }
+    [HttpPost]
+    public IActionResult Delete(int id){
+        Console.WriteLine(id);
+      
+
+       return View(id);
+
+    }
+  
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
